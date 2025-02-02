3bb2bf1de93e951b06ac6d22be5a9424670ddd8f
Matheus-Ludovico <matheusludovico@gmail.com>
2025-02-02 12:48:34 -0300

    Initial commit

---
+++ src/test/java/ludovico/matheus/forumhub/ForumhubApplicationTests.java
@@ -0,0 +1,13 @@
+package ludovico.matheus.forumhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
